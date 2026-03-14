f7aa776e51cada6ae94119903c36f98f4f315128
Ashmika <ashmikapp2002@gmail.com>
2026-03-14 12:34:15 +0530

    updated CRUD functionality

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -19,6 +19,13 @@ export default class Home extends Component {
                             <th>Company</th>
                             <th>Actions</th>
                         </tr>
+                        <tr>
+                          <td><input type='text' value={value.title} onChange={(e)=>{value.updateValue(e,"title")}}/></td>
+                          <td><input type='text' value={value.info} onChange={(e)=>{value.updateValue(e,"info")}}/></td>
+                          <td><input type='text' value={value.price} onChange={(e)=>{value.updateValue(e,"price")}}/></td>
+                          <td><input type='text' value={value.company} onChange={(e)=>{value.updateValue(e,"company")}}/></td>
+                          <td><Button size="sm" onClick={()=>{value.onSave(value.id)}}>{value.id ? "save":"Add" }</Button></td>
+                        </tr>
                         {value.AllData.map(product =>{
                             return (
                                 <tr>
--- src/Contex.js
+++ src/Contex.js
@@ -3,9 +3,91 @@ import { rowData } from './appData'
 
 const ProductContext = React.createContext();
  class ProductProvider extends Component {
-
+  componentDidUpdate(){
+  localStorage.setItem("products", JSON.stringify(this.state.AllData));
+}
   state = {
-    AllData : rowData
+    AllData : JSON.parse(localStorage.getItem("products")) || rowData,
+    id : '',
+    title : '',
+    info : '',
+    company : '',
+    updateEdit : []
+  }
+  getRecord = (id) =>{
+    const product = this.state.AllData.find(item=>item.id === id);
+    return product;
+  }
+  onEdit = (id)=>{
+    const tempProduct = this.state.AllData;
+    const index = tempProduct.indexOf(this.getRecord(id));
+    const slectedRecord = tempProduct[index];
+    this.setState({
+      id: slectedRecord['id'],
+      title: slectedRecord['title'],
+      info: slectedRecord['info'],
+      price: slectedRecord['price'],
+      company: slectedRecord['company']
+      
+    })
+  }
+  updateValue=(e,test)=>{
+    if(test === 'title'){
+      this.state.title = e.target.value
+    }
+
+    if(test === 'info'){
+      this.state.info = e.target.value
+
+    }if(test === 'price'){
+      this.state.price = e.target.value
+
+    }if(test === 'company'){
+      this.state.company = e.target.value
+    }
+    const tempArr = [this.state.id,this.state.title,this.state.info,this.state.price,this.state.company]
+    this.setState({
+      updateEdit : tempArr
+    })
+  }
+  onSave =()=>{
+    if(!this.state.title || !this.state.info || !this.state.price || !this.state.company){
+    alert("Please fill all fields");
+    return;
+  }
+    if(this.state.id!==''){
+      const saveRecord = this.state.AllData;
+      const index = saveRecord.indexOf(this.getRecord(this.state.id));
+      const Record = saveRecord[index]
+      Record['title']=this.state.updateEdit[1];
+      Record['info']=this.state.updateEdit[2];
+      Record['price']=this.state.updateEdit[3];
+      Record['company']=this.state.updateEdit[4];
+      this.setState({
+        AllData :[...this.state.AllData],
+        id:"",title:"",info:"",company : ""
+      })
+    }else{
+      const MaxId = Math.max(...this.state.AllData.map(item=>item.id));
+      const id = MaxId+1;
+      const newArr = {
+  id: id,
+  title: this.state.updateEdit[1],
+  info: this.state.updateEdit[2],
+  price: this.state.updateEdit[3],
+  company: this.state.updateEdit[4]
+};
+      this.setState({
+        AllData :[...this.state.AllData,newArr],
+        id:"",title:"",info:"",company : ""
+      })
+    }
+  }
+  onDelete = (id)=>{
+    const tempProduct = this.state.AllData.filter(item=>item.id!==id);
+    this.setState({
+      AllData:tempProduct
+    })
   }
   render() {
     // console.log(this.state.AllData);
@@ -13,7 +95,12 @@ const ProductContext = React.createContext();
     return (
       <div>
         <ProductContext.Provider
-         value={{...this.state}}
+         value={{...this.state,
+          onEdit : this.onEdit,
+          updateValue :this.updateValue,
+          onSave : this.onSave,
+          onDelete:this.onDelete
+         }}
         >
            {this.props.children}
         </ProductContext.Provider>
